99b31c756fcbeb1fbb60c3f5e62903d06d0df699
Jonh Carvalho <jonh-carvalho@professores.ibmec.edu.br>
2024-10-09 07:51:05 -0300

    Deployed fcae828 with MkDocs version: 1.6.0

---
+++ _Disciplina/code/aula08/index.js
@@ -0,0 +1,23 @@
+document.getElementById('conta').innerHTML = 5 + 5
+
+document.write(5+6)
+document.write('</br>' + (5+7))
+
+console.log(5+6)
+
+a = "10"
+b = 15
+c = (100+50) * a 
+
+console.log(a+b)
+document.write("o valor de a= " + c)
+
+document.getElementById('demo').innerHTML = "Meu Primeiro Javascript"
+
+function Teste() {
+    document.getElementById('teste').innerHTML = "Meu Primeiro Teste";
+}
+
+function Soma(){
+    "document.write(5+6)";
+}
