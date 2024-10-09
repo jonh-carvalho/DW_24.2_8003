26105de1a3368cc9c0370d02df18a6a9bfe41e45
Jonh Carvalho <jonh-carvalho@professores.ibmec.edu.br>
2024-10-09 09:35:27 -0300

    update aula 09

--- docs/_Disciplina/code/aula 09/index.js
+++ docs/_Disciplina/code/aula 09/index.js
@@ -1,3 +1,51 @@
-var x = 10;
-console.log(x)
+/*
+? let x = "John Doe";
+* let x = 0;
+! console.log(x)
+  todo: 
+*/
+
+/*
+function Addittion(num1, num2){
+    a =  num1+num2
+
+    return a
+}
+console.log(Addittion(6.9,9.0))
+*/
+
+/*
+? Variável recebendo uma função
+const add = function Addittion(num1, num2){
+    a =  num1+num2
+    return a
+}
+console.log(add(4,4))
+*/
+
+/*
+let num  =19;
+
+if (num % 2 === 0){
+    console.log("O valor dado é par ")
+} else{
+    console.log("O valor dado é ímpar ")
+}
+*/
+
+/*
+? Condição
+! If else
+! If else if
+
+let num  = -0;
+if (num > 0){
+    console.log("O numero é maior que zero ");
+} else if (num < 0){
+    console.log("O valor dado é menor que zero ")
+} else {
+    console.log("valor  é zero ")
+}
+*/
+
 
