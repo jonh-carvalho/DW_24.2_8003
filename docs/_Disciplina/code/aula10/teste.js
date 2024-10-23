5ce7824c5609961d7daed5bc5d284c3f0ee83d45
Jonh Carvalho <jonh-carvalho@professores.ibmec.edu.br>
2024-10-23 08:01:00 -0300

    update aula

--- docs/_Disciplina/code/aula10/teste.js
+++ docs/_Disciplina/code/aula10/teste.js
@@ -1,12 +1,24 @@
-var student =  { 
-    name : "David Rayy", 
-    sclass : "VI", 
+/* var student =  { 
+    name : "Joe Rayy", 
+    sclass : "I", 
     rollno : 12  };
 
-
-    
   console.log(student);
   delete student.rollno;
   console.log(student.rollno);
   student.rollno = 15;
   console.log(student)
+*/
+
+/* Escreva um programa que cria um botão e
+que adiciona um evento click que imprima uma mensagem de texto no console. 
+*/
+const button = document.createElement('button');
+button.textContent = 'Click me'
+button.addEventListener('click', () => { console.log('Button clicado')})
+
+document.body.appendChild(button);
+
+function clique() {
+  console.log('Button clicado');
+}
